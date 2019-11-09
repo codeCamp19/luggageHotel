3d53bfc95941e52cb8d6a8fb7960dc6d351b1072
Firus Hanov <26045441+ferus2306@users.noreply.github.com>
2019-11-08 18:26:37 -0700

    Add files via upload

---
+++ orders_db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+
+class OrdersDB:
+    def __init__ (self):
+        self.connection = sqlite3.connect("orders_db.db")
+        self.connection.row_factory = dict_factory
+        self.cursor = self.connection.cursor() # cursor is itterator 
+
+    def insertOrder(self, airport, bagAmount, pickUpDate, dropOffDate, comment):
+        data = [airport, bagAmount, pickUpDate, dropOffDate, comment]
+        self.cursor.execute("INSERT INTO orders (airport, bagAmount, pickUpDate, dropOffDate ) VALUES (?,?,?,?,?)", data)    
+        self.connection.commit()
+
+    def getOrders(self):
+        self.cursor.execute("SELECT * FROM orders") # gives list of tuples which is not usable. 
+        result = self.cursor.fetchall()
+        return result
+
+
+    def getOneOrder(self, order_id):
+        data = [order_id]
+        self.cursor.execute("SELECT * FROM orders WHERE id = ?", data)
+        result = self.cursor.fetchone()
+        return result
+
+
+    def deleteOneOrder(self, order_id):
+        data = [order_id]
+        self.cursor.execute("DELETE FROM orders WHERE id = ?", data)
+        self.connection.commit()
+
+
+    def updateOneOrder(self, airport, bagAmount, pickUpDate, dropOffDate, order_id):
+        data = [airport, bagAmount, pickUpDate, dropOffDate, order_id]
+        self.cursor.execute("UPDATE jobs SET airport = ?, bagAmount = ?, pickUpDate = ?, dropOffDate = ? WHERE id = ?", data)
+        self.connection.commit()
+
